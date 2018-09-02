b59f4193d882a2d0150061791d9de17971fad90b
telecoda <robbaines@gmail.com>
2018-09-02 20:56:28 +0100

    Remove unused asti code

--- resources/app/static/js/index.js
+++ resources/app/static/js/index.js
@@ -8,8 +8,6 @@ let index = {
     init: function() {
         // Init
         asticode.loader.init();
-        asticode.modaler.init();
-        asticode.notifier.init();
         
         // Wait for astilectron to be ready
         document.addEventListener('astilectron-ready', function() {
