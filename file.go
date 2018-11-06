b3e026ed20212c74a2b99f88738038259e15c5f4
telecoda <robbaines@gmail.com>
2018-11-06 07:45:51 +0000

    Watch files edited in external editor

--- file.go
+++ file.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/asticode/go-astilectron-bootstrap"
+	"github.com/asticode/go-astilog"
+	"github.com/fsnotify/fsnotify"
+	"github.com/pkg/errors"
 )
+
+var watcher *fsnotify.Watcher
+var cancel chan bool
 
 // load - loads sourcecode from a specific path
 func load(path string) (a Application, err error) {
@@ -13,7 +22,7 @@ func load(path string) (a Application, err error) {
 	// If doesn't end with a filename
 	// look in default location
 	if !strings.HasSuffix(path, ".go") {
-		err = fmt.Errorf("Failed to open file: %s. File MUST bit a .go file", path)
+		err = fmt.Errorf("Failed to open file: %s. File MUST be a .go file", path)
 		return
 	}
 
@@ -28,6 +37,60 @@ func load(path string) (a Application, err error) {
 		err = fmt.Errorf("Failed to read file: %s", err)
 		return
 	}
+
+	if watcher != nil {
+		// close old watcher
+		cancel <- true
+		err = watcher.Close()
+		if err != nil {
+			err = fmt.Errorf("Failed to close previous watcher %s - %s", path, err)
+			return
+		}
+	}
+
+	// creates a new file watcher
+	watcher, err = fsnotify.NewWatcher()
+	if err != nil {
+		return
+	}
+	cancel = make(chan bool)
+	if err = watcher.Add(path); err != nil {
+		return
+	}
+
+	go func(reloadPath string, cancel chan bool) {
+		for {
+			select {
+			// watch for events
+			case event := <-watcher.Events:
+				//fmt.Printf("EVENT! %#v\n", event)
+				if event.Op == fsnotify.Write {
+					// reload
+					if err := bootstrap.SendMessage(w, "reload", reloadPath, func(m *bootstrap.MessageIn) {
+						// Unmarshal payload
+						var s string
+						if m != nil {
+							if err := json.Unmarshal(m.Payload, &s); err != nil {
+								astilog.Error(errors.Wrap(err, "unmarshaling payload failed"))
+								return
+							}
+						}
+					}); err != nil {
+						astilog.Error(errors.Wrap(err, "sending reload event failed"))
+					}
+				}
+
+				// watch for errors
+			case err := <-watcher.Errors:
+				if err != nil {
+					fmt.Printf("ERROR: %s watching for file changes", err)
+				}
+
+			case _ = <-cancel:
+				return
+			}
+		}
+	}(path, cancel)
 
 	// Init Application
 	a = Application{
